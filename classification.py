38b50634e7e75603e5df272b3860a4430536878e
stedestro <stevendestro@users.noreply.github.com>
2017-03-23 11:22:05 +0100

    function headers

--- classification.py
+++ classification.py
@@ -1,6 +1,15 @@
 import math
 
 def _euclidean_distance(p1, p2):
+    '''
+    Calculate the Euclidean disstance between two points
+
+    Arguments:
+        p1,p2: Point in list [x,y,z] or tuple format (x,y,z)
+
+    Return
+        (float) Distance
+    '''
     dim = len(p1)
     if dim != len(p2):
         raise Exception("Points dimension must match")
@@ -10,6 +19,25 @@ def _euclidean_distance(p1, p2):
     return math.sqrt(val)
 
 def knn(training_data, test_data, k, returns_data=False):
+    '''
+    Calculate the k closest neighbours of a point. Classification is done by
+    checking which class of data is more reprensented
+
+    Arguments:
+        training_data (List[List[]]) : The first level contains the different
+            classes to test against, the second level contains an arbitrary
+            number of points. The points are in tuple or list format
+            (e.g. (x,y,z) or [x,y,z])
+        test_data (point) : Point in list or tuple format (see above)
+        k (int) : number of neighbours used to classify test_data
+        returns_data (bool) : if True, add distances to the function's returns
+            otherwise returns the index of calculated class
+
+    Return:
+        (int) index of calculated class
+        (list) if returns_data is set to True,
+            returns [(int)index of calculated[[(int)class, (float)distance, (point)],..]]
+    '''
     nbc = len(training_data) # number of classes
     nn = [] # list containing all distances
 
